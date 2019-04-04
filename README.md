f3f01a7449a07270e1d5abdd77aac5d616b74547
HFRibeiro <ribeirh02@gmail.com>
2019-04-04 10:56:39 +0100

    test and graph

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # AudioRecorder
+```python
+sudo pip3 install matplotlib
+sudo pip3 install scipy
+sudo pip3 install seaborn
+```
+
+```bash
+sudo apt-get install libatlas-base-dev
+```
--- test2.py
+++ test2.py
@@ -1,34 +1,28 @@
 import pyaudio
 import wave
-import time
 
 form_1 = pyaudio.paInt16 # 16-bit resolution
 chans = 1 # 1 channel
 samp_rate = 44100 # 44.1kHz sampling rate 44100 
-chunk = 16384 # 2^12 samples for buffer
+chunk = 4096 # 2^12 samples for buffer
 record_secs = 5 # seconds to record
 dev_index = 2 # device index found by p.get_device_info_by_index(ii)
-wav_output_filename = '/var/www/html/test1.wav' # name of .wav file
+wav_output_filename = 'test1.wav' # name of .wav file
 
 audio = pyaudio.PyAudio() # create pyaudio instantiation
-
-print(audio.get_device_info_by_index(dev_index))
 
 # create pyaudio stream
 stream = audio.open(format = form_1,rate = samp_rate,channels = chans, \
                     input_device_index = dev_index,input = True, \
                     frames_per_buffer=chunk)
 print("recording")
-frames = []
 
-# loop through stream and append audio chunks to frame array
-for ii in range(0,int((samp_rate/chunk)*record_secs)):
+all = []
+for i in range(0, samp_rate / chunk * record_secs):
     data = stream.read(chunk, exception_on_overflow = False)
-    frames.append(data)
+    all.append(data)
+print "* done recording"
 
-print("finished recording")
-
-# stop the stream, close it, and terminate the pyaudio instantiation
 stream.stop_stream()
 stream.close()
 audio.terminate()
@@ -38,5 +32,5 @@ wavefile = wave.open(wav_output_filename,'wb')
 wavefile.setnchannels(chans)
 wavefile.setsampwidth(audio.get_sample_size(form_1))
 wavefile.setframerate(samp_rate)
-wavefile.writeframes(b''.join(frames))
+wavefile.writeframes(b''.join(all))
 wavefile.close()
